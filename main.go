c77796925079c663bc80d2aa8b3f985b258bd5db
megmatic <helloiammegg@gmail.com>
2022-10-29 16:03:47 -0500

    implement for loop

--- main.go
+++ main.go
@@ -6,39 +6,36 @@ func main() {
 	const conferenceName string = "Go Conference"
 	const numOfTotalTickets  int = 50
 	var numOfRemainingTickets uint = 50
-
-	fmt.Printf("numOfTotalTickets is %T, numOfRemainingTickets is %T, conferenceName is %T\n", numOfTotalTickets, numOfRemainingTickets, conferenceName)
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", numOfTotalTickets, numOfRemainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var bookings []string
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets int
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets int
 
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
 
-	fmt.Println("How many tickets do you want to reserve: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("How many tickets do you want to reserve: ")
+		fmt.Scan(&userTickets)
 
-	numOfRemainingTickets = numOfRemainingTickets - uint(userTickets);
-	bookings = append(bookings, firstName + " " + lastName)
+		numOfRemainingTickets = numOfRemainingTickets - uint(userTickets);
+		bookings = append(bookings, firstName + " " + lastName)
 
-	fmt.Printf("The whole slice: %v\n", bookings)
-	fmt.Printf("The first value: %v\n", bookings[0])
-	fmt.Printf("Slice type: %T\n", bookings)
-	fmt.Printf("Slice length: %v\n", len(bookings))
+		fmt.Printf("Thank you, %v %v, we reserved %v tickets for you.\nYou'll receive a confirmation email at %v shortly.\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", numOfRemainingTickets, conferenceName)
 
-	fmt.Printf("Thank you, %v %v, we reserved %v tickets for you.\nYou'll receive a confirmation email at %v shortly.\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", numOfRemainingTickets, conferenceName)
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
 }
