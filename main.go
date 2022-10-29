6f27108f96cd91618d16539979ce8dd560365806
megmatic <helloiammegg@gmail.com>
2022-10-29 15:05:50 -0500

    implementing an array

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", numOfTotalTickets, numOfRemainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var bookings [50]string
 	var firstName string
 	var lastName string
 	var email string
@@ -31,6 +32,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	numOfRemainingTickets = numOfRemainingTickets - uint(userTickets);
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
 
 	fmt.Printf("Thank you, %v %v, we reserved %v tickets for you.\nYou'll receive a confirmation email at %v shortly.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", numOfRemainingTickets, conferenceName)
